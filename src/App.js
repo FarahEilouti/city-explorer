1f4ce41c0bd6c7fb62eae9972c89e01cc63ef91a
Farah Riad Eilouti <fara7.eilouti@gmail.com>
2022-08-29 01:15:39 +0300

    lab6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+class App extends React.Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props){
+    super(props);
+    this.state={
+      display_name:'',
+      lat:'',
+      lon:'',
+      error : 'sorry something',
+      errorFlag : false,
+      imgerrorFlag : false
+    }
+  }
+
+  gitLocData = async (e)=>{
+    e.preventDefault();
+    const cityName = e.target.city.value;
+    let key = 'pk.ee6de1fe66889b1faeffc4e181a923c6';
+    const Url = `https://eu1.locationiq.com/v1/search?key=${key}&q=${cityName}&format=json`
+    try{
+      let resResult = await axios.get(Url)
+
+
+      this.setState({
+        display_name : resResult.data[0].display_name,
+        lat : resResult.data[0].lat,
+        lon : resResult.data[0].lon,
+        image : `https://maps.locationiq.com/v3/staticmap?key=pk.1ed11f75d75e6a8c33f7c8e4afae6908&center=${resResult.data[0].lat},${resResult.data[0].lon}`
+      })
+    }catch{
+      console.log('Error')
+      this.setState({
+        errorFlag:true
+      })
+    }
+
+  }
+  render(){
+
+    return(
+      <div>
+        <h1>location</h1>
+        <form onSubmit={this.gitLocData}>
+          <input type="text" name="city" placeholder='Enter a city' ></input>
+          <button type='submit'>Submit</button> 
+        </form>
+        <h3>Display name: {this.state.display_name} </h3>
+        <p>Lon : {this.state.lon}</p>
+        <p>Lat : {this.state.lat}</p>
+
+        {this.state.errorFlag && <h4>Error 404 : {this.state.error}</h4>}
+        <img src= {this.state.image} />
+      </div>
+    )
+  }
 }
-
 export default App;
